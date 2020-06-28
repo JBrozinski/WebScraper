c835fe573c1a477ff18169a5c33034be2bd4e6b9
JBrozinski <joseph.brozinski@gmail.com>
2020-06-28 14:43:49 -0600

    added functionality to index.js page

---
+++ public/assets/javascript/saved.js
@@ -0,0 +1,120 @@
+const e = require("express");
+
+$(document).ready(function () {
+    var articleContainer = $(".article-container");
+
+    $(document).on("click", ".btn.delete", handleArticleDelete);
+    $(document).on("click", ".btn.notes", handleArticleNotes);
+    $(document).on("click", ".btn.save" handleNoteSave);
+    $(document).on("click", ".btn.note-delete", handleNoteDelete);
+
+    initPage();
+
+    function initPage() {
+        articleContainer.empty();
+        $.get("/api/headlines?saved=true").then(function (data) {
+            if (data && data.length) {
+                renderArticles(data);
+            } else {
+                renderEmpty();
+            }
+        });
+    }
+
+    function renderArticles(articles) {
+        var articlePanels = [];
+        for (var i = 0; i < articles.length; i++) {
+            articlePanels.push(createPanel(articles[i]));
+        }
+        articleContainer.append(articlePanels);
+    }
+
+    function createPanel(article) {
+        var panel =
+            $([
+            < "div class='panel panel-default'>",
+                "<div class ='panel-heading'>",
+                "<h3>",
+                article.headline,
+                "<a class='btn btn-danger delete'>",
+                "Delete from Saved",
+                "</a>",
+                "<a class='btn btn-info notes'>Article Notes</a>",
+                "</h3>",
+                "/div>",
+                "<div class='panel-body'>",
+                article.summary,
+                "</div>",
+                "</div>"
+
+            ].join(""));
+        panel.data("_id", article.id);
+        return panel;
+    }
+
+    function renderEmpty() {
+        var emptyAlert =
+            $(["<div class='alert alert-warning text-center'>",
+                "<h4>'Sorry, there are no new articles'</h4>",
+                "</div>",
+                "<div class='panel panel-default'>",
+                "<div class'panel-heading text-center'>",
+                "<h3>Browse Available Articles?</h3>",
+                "</div>",
+                "<div class='panel-body text-center'>",
+                "<h4><a href='/'>Browse Articles</a></h4>",
+                "</div>",
+                "</div>"
+            ].join(""));
+        articleContainer.append(emptyAlert);
+    }
+
+    function renderNotesList(data) {
+        var notesToRender = [];
+        var currentNote;
+        if (!data.notes.length) {
+            currentNote = [
+
+            ]
+        }
+    }
+
+    function handleArticleDelete() {
+        var articleToDelete = $(this).parents(".panel").data();
+        $.ajax({
+            method: "DELETE",
+            url: "/api/headlines/" + articleToDelete._id
+        }).then(funtion(data) {
+            if(data.ok) {
+            initPage();
+        }
+    });
+    }
+
+function handleArticleNotes() {
+    var currentArticle = $(this).parents(".panel").data();
+    $.get("/api/notes/" + currentArticle._id).then(function (data) {
+        var modalText = [
+            "<div class='container-fluid text-center'>",
+            "<h4>Notes For Article: ",
+            currentArticle._id,
+            "</h4>",
+            "<hr />",
+            "<ul class='list-group note-container'>",
+            "</ul>",
+            "<textarea placeholder='New Note, rows='4' cols='60'></textarea>",
+            "<button class='btn btn-success save'>Save Note</button>",
+            "</div>"
+        ].join("");
+        bootbox.dialog({
+            message: modalText,
+            closeButton: true
+        });
+        var noteData = {
+            _id: currentArticle._id,
+            notes: data || []
+        };
+        $(".btn.save").data("article", noteData);
+    });
+}
+})
