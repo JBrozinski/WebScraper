e6a16518742de40e333ddfef12ccd986de1d5760
JBrozinski <joseph.brozinski@gmail.com>
2020-07-01 15:57:21 -0600

    got cheerio to scrape

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,13 +1,14 @@
 var request = require("request");
 var cheerio = require("cheerio");
+
 var scrape = function (cb) {
 
-    request("https://www.bbc.com/news/coronavirus", function (err, res, body) {
+    request("https://coinrivet.com/category/bitcoin-news/", function (err, res, body) {
         var $ = cheerio.load(body);
         var articles = [];
-        $(".gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor").each(function (i, element) {
-            var head = $(this).children(".gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text").text().trim();
-            var sum = $(this).children("gs-c-promo-summary gel-long-primer gs-u-mt nw-c-promo-summary gs-u-display-none gs-u-display-block@m").text().trim();
+        $(".t").each(function (i, element) {
+            var head = $(this).children().text();
+            var sum = $(this).children().attr("href");
 
             if (head && sum) {
                 var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
@@ -21,6 +22,7 @@ var scrape = function (cb) {
             }
         });
         cb(articles);
+        console.log(articles);
     });
 };
 
